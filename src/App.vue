7507bce6df6264118c5e3cf54be4b6fad5bf8518
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-03 18:22:22 +0300

    initial files

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import NavBar from './components/NavBar.vue';
+import TaskPage from './pages/TaskPage.vue';
+</script>
+
+<template>
+  <NavBar/>
+  <TaskPage/>
+</template>
+
+<style scoped>
+
+</style>
