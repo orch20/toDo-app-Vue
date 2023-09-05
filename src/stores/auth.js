186e4727c030e3a0a1a9281a7ebafa2bcfd42cfa
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-05 16:41:33 +0300

    created a Pinia store for authentification

---
+++ src/stores/auth.js
@@ -0,0 +1,41 @@
+import { defineStore } from 'pinia'
+import { ref, computed } from 'vue'
+import { csrfCookie, login, register, logout, getUser } from '../http/auth-api'
+
+export const useAuthStore = defineStore('authStore', () => {
+  const user = ref(null)
+
+  const isLoggedIn = computed(() => !!user.value)
+
+  const fetchUser = async () => {
+    const { data } = await getUser()
+    user.value = data
+  }
+
+  const handelLogin = async (credentials) => {
+    await csrfCookie()
+    await login(credentials)
+    await fetchUser()
+  }
+
+  const handelRegister = async (newUser) => {
+    await register(newUser)
+    await handelLogin({
+      email: newUser.email,
+      password: newUser.password
+    })
+  }
+
+  const handelLogout = async () => {
+    await logout()
+    user.value = null
+  }
+  return {
+    user,
+    isLoggedIn,
+    fetchUser,
+    handelLogin,
+    handelRegister,
+    handelLogout
+  }
+})
