6202fa15b62c57b2df523e63d89c8a2892ca4ee5
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-06 15:30:28 +0300

    handeled vaidation error messages

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -4,6 +4,7 @@ import { csrfCookie, login, register, logout, getUser } from '../http/auth-api'
 
 export const useAuthStore = defineStore('authStore', () => {
   const user = ref(null)
+  const errors = ref({})
 
   const isLoggedIn = computed(() => !!user.value)
 
@@ -19,8 +20,16 @@ export const useAuthStore = defineStore('authStore', () => {
 
   const handelLogin = async (credentials) => {
     await csrfCookie()
-    await login(credentials)
-    await fetchUser()
+
+    try {
+      await login(credentials)
+      await fetchUser()
+      errors.value = {}
+    } catch (error) {
+      if (error.response && error.response.status === 422) {
+        errors.value = error.response.data.errors
+      }
+    }
   }
 
   const handelRegister = async (newUser) => {
@@ -38,6 +47,7 @@ export const useAuthStore = defineStore('authStore', () => {
   return {
     user,
     isLoggedIn,
+    errors,
     fetchUser,
     handelLogin,
     handelRegister,
