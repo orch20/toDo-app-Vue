e3fc3ef78c76fb88399f7dccf68bb40f91ea4389
orch20 <94862153+orch20@users.noreply.github.com>
2023-09-06 10:54:46 +0300

    added login form

--- src/http/auth-api.js
+++ src/http/auth-api.js
@@ -2,10 +2,10 @@ import api from './api.js'
 
 export const csrfCookie = () => api.get('/sanctum/csrf-cookie')
 
-export const login = (credentials) => api.post('/auth/login', credentials)
+export const login = (credentials) => api.post('api/auth/login', credentials)
 
 export const register = (user) => api.post('/auth/register', user)
 
 export const logout = () => api.post('/auth/logout')
 
-export const getUser = () => api.get('/auth/user')
+export const getUser = () => api.get('/user')
